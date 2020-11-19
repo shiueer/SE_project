31e625896a1cb1a7f84acd63b19155f3756aadc6
學而\eric8 <eric89425@gmail.com>
2020-11-19 18:28:41 +0800

    fix db conecting

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $user = 'test';
 $pass = '';
-$db = 'test';
+$db = 'se_project';
 $conn = mysqli_connect($host, $user, $pass, $db) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
 mysqli_query($conn,"SET NAMES utf8"); //選擇編碼
 ?>
