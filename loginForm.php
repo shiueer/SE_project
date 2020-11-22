1e504fca51df0d864f353977221b860467c17583
學而\eric8 <eric89425@gmail.com>
2020-11-23 00:14:07 +0800

    correct the way of checking role's value

--- loginForm.php
+++ loginForm.php
@@ -2,18 +2,13 @@
 session_start();
 //set the login mark to empty
 $_SESSION['uID'] = "";
+$_SESSION['rID'] = "";
 ?>
 <h1>Login Form</h1><hr />
 <form method="post" action="loginCheck.php">
 User Name: <input type="text" name="id"><br />
 <br/>
 Password : <input type="password" name="pwd"><br />
-<br/>
-Role :
-<input type="radio" name="role" value="0"><label>student</label>
-<input type="radio" name="role" value="1"><label>teacher</label>
-<input type="radio" name="role" value="2"><label>secretary</label>
-<input type="radio" name="role" value="3"><label>principal</label><br/>
 <br/>
 <input type="submit">
 </form>
